95636217287d71a8925d3b968fc1f4b133af14fa
Vagner Gonçalves <vlgonalves@gmail.com>
2016-07-26 17:22:13 -0300

    Pre teste de Api

---
+++ app/src/main/java/ggtec/lei_concursospublicos/teste.java
@@ -0,0 +1,13 @@
+package ggtec.lei_concursospublicos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class teste extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_teste);
+    }
+}
